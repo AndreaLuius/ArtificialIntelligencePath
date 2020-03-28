857a4b75d335626779b1cdceb33a28e7b08d94fc
AndreaLuius <andrea.luiu@libero.it>
2020-03-28 18:33:40 +0100

    added the movementAi class to make the enemy move around the map and reset all the positions

---
+++ Assets/scripts/EnemyMovementAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovementAI : MonoBehaviour
+{
+    [SerializeField] List<WayPoint> path;
+        
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(executionPath());
+    }
+    
+    private IEnumerator executionPath()
+    {
+        foreach(var index in path)
+        {
+            transform.position = index.transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
